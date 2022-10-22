c7063a85c2fe168c38d313be15af93487ed61dd8
Rajneesh Chaudhary <113371031+ChItachi@users.noreply.github.com>
2022-10-22 20:40:27 +0530

    Create pattern.cpp

---
+++ pattern.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int size=2*n-1;
+    int start=0;
+    int end=size-1;
+    int arr[size][size];
+    for(int i=0;i<n;n--){
+        for(int r=start;r<=end;r++){
+            for (int j=start;j<=end;j++){
+                if(r == start  r== end  
+                    j==start || j==end)
+                    arr[r][j]=n;
+            }
+        }
+        ++start;
+        --end;
+    }
+
+
+    for(int i=0;i<size;i++){
+        for (int j=0;j<size;j++){
+            printf("%d",arr[i][j] );
+        }
+        printf("\n");
+    }
+}
